22f256fde3ef6226655ed06f16ef5fd476a18ba3
Peavey <11081113+peavey2787@users.noreply.github.com>
2026-01-22 12:19:28 -0500

    wip: kaspa wrapper facades

--- wrapper/intelligence/intelligenceFacade.js
+++ wrapper/intelligence/intelligenceFacade.js
@@ -1,5 +1,6 @@
 import { walkDagToPresent, scanDagForward, scanDagBackward } from './dag_walker.js';
 import { KaspaBlockScanner } from './scanner.js';
+import { IndexerEventType, EvictionReason } from './indexer.js';
 
 export class IntelligenceFacade {
   /**
@@ -9,12 +10,17 @@ export class IntelligenceFacade {
    */
   constructor(client, scannerOptions = {}, indexerOptions = {}) {
     this.client = client;
+    this._callbacks = {};
+
+    const onIndexerUpdate = (event) => {
+      this._handleIndexerUpdate(event);
+    };
 
     // The Scanner is the "Worker" - it creates and owns the Indexer
     // We pass the indexerOptions straight through as the scanner expects.
     this.scanner = new KaspaBlockScanner(client, {
       ...scannerOptions,
-      indexerOptions: indexerOptions
+      indexerOptions: { ...indexerOptions, onIndexerUpdate }
     });
 
     // Expose the indexer for direct queries (getMetrics, getAllCachedBlocks, etc.)
@@ -22,6 +28,56 @@ export class IntelligenceFacade {
     
     this._activeTasks = new AbortController();
   }
+
+  _handleIndexerUpdate = (event) => {
+    const { type, data } = event;
+
+    switch (type) {
+      case IndexerEventType.TRANSACTION_IN_MEMORY:
+        this._trigger('onNewTransaction', data);
+        break;
+      case IndexerEventType.MATCHING_TRANSACTION_IN_MEMORY:
+        this._trigger('onNewTransactionMatch', data);
+        break;
+      case IndexerEventType.BLOCK_IN_MEMORY:
+        this._trigger('onNewBlock', data);
+        break;
+      case IndexerEventType.TRANSACTION_CACHED:
+        this._trigger('onCachedTransaction', data);
+        break;
+      case IndexerEventType.MATCHING_TRANSACTION_CACHED:
+        this._trigger('onCachedTransactionMatch', data);
+        break;
+      case IndexerEventType.BLOCK_CACHED:
+        this._trigger('onCachedBlock', data);
+        break;
+      case IndexerEventType.EVICT:
+        // Differentiate between cache evictions and full evictions
+        if (data?.reason === EvictionReason.TTL || data?.reason === EvictionReason.SIZE) {
+          this._trigger('onCacheEvict', data);
+        } else {
+          this._trigger('onEvict', data);
+        }
+        break;
+      default:
+        console.warn("IntelligenceFacade: Unknown event type:", type);
+    }
+  };
+
+  _trigger(name, data) {
+    if (typeof this._callbacks[name] === 'function') {
+      this._callbacks[name](data);
+    }
+  }
+
+  onNewBlock(cb) { this._callbacks.onNewBlock = cb; return this; }
+  onNewTransaction(cb) { this._callbacks.onNewTransaction = cb; return this; }
+  onNewTransactionMatch(cb) { this._callbacks.onNewTransactionMatch = cb; return this; }
+  onCachedBlock(cb) { this._callbacks.onCachedBlock = cb; return this; }
+  onCachedTransaction(cb) { this._callbacks.onCachedTransaction = cb; return this; }
+  onCachedTransactionMatch(cb) { this._callbacks.onCachedTransactionMatch = cb; return this; }
+  onEvict(cb) { this._callbacks.onEvict = cb; return this; }
+  onCacheEvict(cb) { this._callbacks.onCacheEvict = cb; return this; }
 
   /**
    * Starts the system. 
